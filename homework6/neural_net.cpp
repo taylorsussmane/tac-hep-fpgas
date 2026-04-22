48d9e81f0ef756efc979304898a979a5082d7971
Taylor Sussmane <tsussmane1@gmail.com>
2026-04-22 16:30:09 -0500

    completed hw6

--- homework6/neural_net.cpp
+++ homework6/neural_net.cpp
@@ -3,11 +3,15 @@
 #define J 8
 
 void neural_net(int in[I][1], int out[J][1], int weight[J][I]){
+	#pragma HLS array_partition variable=in complete
+	#pragma HLS array_partition variable=out complete
+	#pragma HLS array_partition variable=weight complete
 
 	for (int i = 0; i < I; i++){
 		for (int j = 0; j < J; j++){
 			int sum = 0;
 			for (int k = 0; k < I; k++){
+				#pragma HLS loop_flatten
 				sum += in[k][0]*weight[j][k];
 			}
 			out[j][0] = sum;
